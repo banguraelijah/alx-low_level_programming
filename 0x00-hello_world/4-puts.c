24484ff4d6d0b2f376c2d731724c07eaaa3ac7c9
banguraelijah <banguraelijah28@gmail.com>
2022-09-08 11:17:25 -0700

    C program that prints using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#includes <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Return: 0 after running the function
+ */
+
+int main(void)
+{puts("\""Programming is like building a multilingual puzzle");
+	   return(0);
+}
