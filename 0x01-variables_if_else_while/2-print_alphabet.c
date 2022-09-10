444150afa3d8d29556190ef6b9ba15ed28c446f9
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 06:35:53 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - runs the code when ever it is excuted
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int x = 'a'
+while (x <= 'z')
+{
+putchar(tolower(x));
+x++;
+}
+return (0);
+}
