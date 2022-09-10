604a406d398f99efb4aa52c9f7d43224b19b4c14
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 10:01:40 -0700

    print alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.>
+/**
+ * main - runs every time it is excuted
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int x = 'a';
+while (x <= 'z')
+{
+if (x == 'q' || x == 'e')
+{
+x++;
+}
+else
+{
+putchar(x);
+x++
+}
+}
+putchar('\n')
+return (0);
+}
