d170deb9630ddb07b2b3feaa0c7ed2d41ce53c5c
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 04:29:41 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
 if (last > 5)
-printf("Last digit of n: %d is %d and is greater than 5\n", n);
+printf("Last digit of n: %d is %d and is greater than 5\n", n, last);
 else if (last == 0)
-printf("Last digit of n: %d is %d and is zero\n", n);
+printf("Last digit of n: %d is %d and is zero\n", n, last);
 else
-printf("Last digit of n: %d is %d and is less than 6 and not 0\n", n);
+printf("Last digit of n: %d is %d and is less than 6 and not 0\n", n, last);
 return (0);
 }
