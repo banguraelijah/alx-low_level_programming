301f6f9125d71680c6dcf3f22c2e774f64021891
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 12:13:07 -0700

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - runs the code when executed
+ *
+ * Return: always 0 (success)
+ */
+int n;
+int letter = 'a';
+while (n < 10)
+{
+putchar((num % 10) + '0');
+n++;
+}
+while (letter <= 'f')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
