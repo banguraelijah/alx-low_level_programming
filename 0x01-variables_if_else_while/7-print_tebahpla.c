4d73316ad74cde911741dedb50a329e0292d98c8
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 11:49:46 -0700

    print alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - runs the code when executed
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int n = 'z'
+while (n >= 'a')
+{
+putchar(n);
+n = n - 1;
+}
+putchar('\n');
+return (0);
+}
