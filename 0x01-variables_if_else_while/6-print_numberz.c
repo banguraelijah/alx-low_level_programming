e662a6b9248b30ab45a811bd00116dbae89c556d
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 10:36:53 -0700

    print base 10 of single numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - runs the code when ever it is excuted
+ *
+ * Return: always 0 (success)
+ */
+int main(void);
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+putchar('\n');
+return (0);
+}
