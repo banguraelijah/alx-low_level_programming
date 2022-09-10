d5e966ee131fc217c0e283072a3b41f8b885e4fb
banguraelijah <banguraelijah28@gmail.com>
2022-09-10 11:20:16 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - runs the code when executed
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+putchar('\n');
+return (0);
+}
