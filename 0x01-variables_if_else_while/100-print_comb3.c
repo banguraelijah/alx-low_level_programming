5028cd2d81fe31073ee64120a75bd98ce2236872
banguraelijah <banguraelijah28@gmail.com>
2022-09-11 12:13:09 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-int digit1;
-int digit2;
-while (digit1 < 10)
+int digit1, digit2;
+for (digit1 = 0; digit1 < 10; digit1++)
 {
-while (digit2 < 10)
+for (digit2 = 0; digit2 < 10; digit2++)
 {
 putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+putchar((digit2 % 10) + '0');
 if (digit1 == 9 && digit2 == 9)
 continue;
 putchar(',');
