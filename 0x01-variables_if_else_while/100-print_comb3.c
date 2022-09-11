1fc8304f1f96cf1dbbc82824955507a8a50b85ce
banguraelijah <banguraelijah28@gmail.com>
2022-09-11 15:54:07 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 int c, i;
 c = i = '0';
-for (c = '0'; c <= '57'; c++)
+for (c = '0'; c <= '9'; c++)
 {
-for (i = '0'; i <= '57'; i++)
+for (i = '0'; i <= '9'; i++)
 {
 putchar(c);
 putchar(i);
-if ((c != '57') || (c == '57' && i != '57'))
+if ((c != '9') || (c == '9' && i != '9'))
 {
 
 putchar(',');
