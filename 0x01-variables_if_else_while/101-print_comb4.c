f618dcbbdd579c362ff02917a161b4280067ba9b
banguraelijah <banguraelijah28@gmail.com>
2022-09-11 17:42:41 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,31 @@
  */
 int main(void)
 {
-int n1;
-int n2;
-int n3;
-
+int n1 = 0, n2, n3;
+while (n1 <= 9)
+{
+n2 = 0;
+while (n2 <= 9)
+{
+n3 = 0
+while (n3 <= 9)
+{
+if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)
+{
+putchar(n1);
+putchar(n2);
+putchar(n3);
+if (n1 + n2 + n3 != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+n3++
+}
+n2++
+}
+n1++
+}
+putchar('\n');
+return (0);
