c21a39f7d53cbe79d51452366f1cb26cad5d9e05
banguraelijah <banguraelijah28@gmail.com>
2022-09-11 18:36:44 -0700

    print 3 combination of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print 3 combination of numbers
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+int n1 = 0, n2;
+while (n1 <= 99)
+{
+n2 = n1;
+while (n2 <= 99)
+{
+if (n1 != n2)
+{
+putchar((n1 / 10) + 48);
+putchar((n1 % 10) + 48);
+putchar(' ');
+putchar((n2 / 10) + 48);
+putchar((n2 % 10) + 48);
+if (n1 != 98 || n2 != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+n2++;
+}
+n1++;
+}
+putchar('\n')
+return (0);
+}
