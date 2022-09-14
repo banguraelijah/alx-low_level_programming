b56881e537210c48b97d7e4593ad233a66c564c2
banguraelijah <banguraelijah28@gmail.com>
2022-09-14 01:50:26 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,15 @@ void times_table(void)
 	_putchar(',');
 	_putchar(' ');
 	prod = num1 * num2;
-	_putchar(prod);
 
+	if (prod <= 9)
+{
+	_putchar(' ');
 }
+else
+	_putchar(prod / 10 + '0');
+	_putchar(prod % 10 + '0')
 }
-_putchar('\n');
+_putchar('\n')
+}
 }
