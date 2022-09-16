f18b227537135ce9095204e6462904470361a49a
banguraelijah <banguraelijah28@gmail.com>
2022-09-16 10:09:38 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print a square in the terminal
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+{
+else
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; i++)
+{
+	_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
