beded73989b4fa19fa8524ef89290e8b9faadf4f
banguraelijah <banguraelijah28@gmail.com>
2022-09-16 05:23:08 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,7 +10,8 @@ void print_numbers(void)
 	int c;
 
 	for (c = '0'; c <= 9; c++)
-	_putchar((c % 10) + '0');
-
+{
+	_putchar(c);
+}
 	_putchar('\n'); 
 }
