4e1fef950cfa45f8c4e38dc13065fc5c89ea53fd
banguraelijah <banguraelijah28@gmail.com>
2022-09-19 13:13:25 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the value of integres
+ * @a: int to swap
+ * @b: int to swap
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	a = b;
+}
