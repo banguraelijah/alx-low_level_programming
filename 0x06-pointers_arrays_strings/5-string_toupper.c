246f6b0f302d5f6653d6f9ab78c4c444cd46a091
banguraelijah <banguraelijah28@gmail.com>
2022-09-22 12:52:33 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,21 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * string_toupper - converts the whole string to uppercase
- * @str: string to be converted
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
  *
- * Return: string
+ * Return: the resulting string
  */
 char *string_toupper(char *str)
 {
-	int length = strlen(str);
+	int i;
 
-	for (i = 0; i < length; i++)
+	for (i = 0; str[i] != '\0'; i++)
 {
-	str[i] = toupper(str[i]);
+	if (str[i] >= 'a' && str[i] <= 'z')
+	str[i] = str[i] - 32;
 }
+
 	return (str);
 }
---
+++ 0x06-pointers_arrays_strings/to_upper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - converts the whole string to uppercase
+ * @str: string to be converted
+ *
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int length = strlen(str);
+
+	for (i = 0; i < length; i++)
+{
+	str[i] = toupper(str[i]);
+}
+	return (str);
+}
