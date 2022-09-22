f446ddb0760e63de4010b65fdeaf7d7260d3cd3a
banguraelijah <banguraelijah28@gmail.com>
2022-09-22 16:16:41 -0700

    something ive got to look at more

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *s);
-char *leet(char *);
+char *leet(char *s);
 char *rot13(char *);
 void print_number(int n);
 
