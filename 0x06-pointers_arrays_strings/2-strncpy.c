29fb8a3e167182db25b0f2c70d91165427eeae00
banguraelijah <banguraelijah28@gmail.com>
2022-09-22 10:00:15 -0700

    char *_strncpy(char *dest, char *src, int n)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies specific numbers of characters in a string
+ * @dest: string to be used<F9>
+ * @src: string to copy
+ * @n: length of string to be copied
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, len1, len2;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+
+	for (i = 0; len2 && i < n; i++)
+{
+	dest[len1++] = src[i];
+{
+	for (i = 0; i < n; i++)
+{
+	dest[i] = '\0';
+
+}
+}
+}
+return (dest);
+}
