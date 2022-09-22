a9e83b7347646b0b344cc8ea9faae4f2bcaa91b7
banguraelijah <banguraelijah28@gmail.com>
2022-09-22 09:21:48 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strncat.c -o 1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: string to be used
+ * @src: string to ne concatenated
+ * @n: the limit for \0 to be added
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1, len2, i;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+
+	for (i = 0; len2 != '\0' && i < n; i++)
+{
+	dest[len1++] = src[i];
+}
+return (dest);
+}
