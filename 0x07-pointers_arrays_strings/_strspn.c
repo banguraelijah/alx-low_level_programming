00ebcb6557c34b0b1e0759ace49300e032b1f55b
banguraelijah <banguraelijah28@gmail.com>
2022-09-26 22:18:42 -0700

    check for chsrscters that are present in both strings

---
+++ 0x07-pointers_arrays_strings/_strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - length of a prefix substring
+ * @s: string to check for
+ * @accept:nput
+ *
+ * Return: 0 on success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int initial_length = 0:
+	
+
