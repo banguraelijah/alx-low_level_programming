be117f469502095872a51cabaf4abe77e3d69398
banguraelijah <banguraelijah28@gmail.com>
2022-09-29 07:49:21 -0700

    checks if a number is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+ * prime_number - checks for prime number
+ * @n: the prime number
+ * @i: checks the numbers if prime
+ *
+ * Return: depends on the statement
+ */
+int prime_number(int n, int i)
+{
+	if (i == 1)
+	return (1);
+	if (n % i == 0 && i > 1)
+	return (0);
+	return (prime_number(n, i - 1));
+}
+/**
+ * is_prime_number - check if a number is prime
+ * @n: the number to check for
+ *
+ * Return: depends on statement
+ */
+int is_prime_number(int n)
+{ 
+	if (n <= 1)
+	return (0);
+	return (prime_number(n, n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int power_maths(int n, int c);
 int _sqrt_recursion(int n);
+int prime_number(int n, int i);
+int is_prime_number(int n);
 
 
 #endif
