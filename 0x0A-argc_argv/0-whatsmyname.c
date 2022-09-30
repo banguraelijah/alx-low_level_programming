ecd8cd2c6e23f85ccbaf64a098163ce75dda3212
banguraelijah <banguraelijah28@gmail.com>
2022-09-30 03:07:02 -0700

    printsw name followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @argc: number of things entered on the command line
+ * @argv: an array that holds the string values ente4red on the command line
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
