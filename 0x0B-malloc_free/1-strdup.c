e1933792ad9c6a79ce323fa0e5027ae0b040b805
banguraelijah <banguraelijah28@gmail.com>
2022-10-03 16:27:09 -0700

    return a pointer to a newly allocated string with a copy of the string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,11 +19,12 @@ char *_strdup(char *str)
 	return ('\0');
 	while (*(str + length_of_str) != '\0')
 		length_of_str++;
+	length_of_str++;
 	str_cpy = malloc(*(str) * length_of_str);
 	if (str_cpy == NULL)
 	return ('\0');
 	for (i = 0; i < length_of_str; i++)
-	str_cpy[i] = *(str);
+	str_cpy[i] = *(str + i);
 	str_cpy[i] = '\0';
 	return (str_cpy);
 }
