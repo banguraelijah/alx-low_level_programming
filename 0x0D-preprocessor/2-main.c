7dbb65d80013348ab84a1182c77bd242c82ea338
banguraelijah <banguraelijah28@gmail.com>
2022-10-11 16:32:35 -0700

    Task1, Task2, Task 0

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  prints the name of the file it was compiled from, followed by a new line
+ *
+ * Return: 0 ON SUCCESS
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
